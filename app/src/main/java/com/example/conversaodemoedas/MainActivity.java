22df865abb86962efdc31fa8a58f6a993d13615b
Iuiza <luiza.floquinho@gmail.com>
2024-10-15 14:58:07 -0300

    finishing app

--- app/src/main/java/com/example/conversaodemoedas/MainActivity.java
+++ app/src/main/java/com/example/conversaodemoedas/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.conversaodemoedas;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editQuantidadeDolar;
+    private EditText editCotacaoDolar;
+    private TextView textResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editQuantidadeDolar = findViewById(R.id.editQuantidadeDolar);
+        editCotacaoDolar = findViewById(R.id.editCotacaoDolar);
+        textResultado = findViewById(R.id.textResultado);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void converter(View view){
+        double quantidadeDolar = Double.parseDouble(editQuantidadeDolar.getText().toString());
+        double cotacaoDolar = Double.parseDouble(editCotacaoDolar.getText().toString());
+
+        double resultado = quantidadeDolar * cotacaoDolar;
+
+        textResultado.setText("Valor convertido em Real: " + resultado);
     }
 }
